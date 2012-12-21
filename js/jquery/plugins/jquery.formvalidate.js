e819f5dd397d626b8ff6c4fb3f1a21e4f255d727
Alvaro José Agámez Licha <alvaroagamez@outlook.com>
2012-12-20 23:23:35 -0500

    Code refactoring and new data validator methods
    New data validator methods
    isValidEmail()
    isValidInteger()
    isValidNumber()

--- js/jquery/plugins/jquery.formvalidate.js
+++ js/jquery/plugins/jquery.formvalidate.js
@@ -4,64 +4,8 @@
  */
 
 (function($) {
+    'use strict';
 
-    /**
-     * 
-     * @param {DOMElement} element The element to validate
-     * @param {DOMElement form The form that contains the element to validate
-     * @returns {boolean} TRUE on error, FALSE otherwise
-     */
-    function validateRequired(element, form) {
-        switch (element.type) {
-            case 'checkbox': 
-            case 'radio': {
-                if (0 === $(':input[name="' + element.name + '"]:checked', form).length) {
-//                    alert('ERROR: ' + element.name + ' is required');
-                    showError(element, 'required');
-                    return true;
-                };
-                
-                return false;
-                break;
-            }
-
-            case 'password': 
-            case 'textarea': 
-            case 'text': {
-                if ('' === $.trim($(element).val())) {
-                    showError(element, 'required');
-                    return true;
-                }
-
-                return false;
-                break;
-            }
-
-            case 'select-one': {
-                if ('' === $('option:selected', element).val()) {
-                    showError(element, 'required');
-                    return true;
-                }
-                
-                return false;
-                break;
-            }
-
-            case 'select-multiple': {
-                if (0 === $('option:selected', element).length) {
-                    showError(element, 'required');
-                    return true;
-                }
-
-                return false;
-                break;
-            }
-        }
-    }
-
-    function validateEmail() {
-        
-    }
 
     /**
      * Hide the error message for an element
@@ -75,6 +19,119 @@
     }
 
     /**
+     * Validates whether the value of an element is an email
+     * 
+     * @param {DOMElement} element
+     * @param {DOMElement} form
+     * @returns {boolean}
+     */
+    function isValidEmail(element, form) {
+        var emailPattern = /(^[0-9a-zA-Z]+(?:[._][0-9a-zA-Z]+)*)@([0-9a-zA-Z]+(?:[._-][0-9a-zA-Z]+)*\.[0-9a-zA-Z]{2,3})$/;
+
+        try {
+            if (!emailPattern.test(element.value)) {
+                showError(element, 'email');
+                return false;
+            }
+
+            return true;
+        } catch (Exception){
+            return false;
+        }
+    }
+
+    /**
+     * Validates whether the value of an element is an integer
+     * 
+     * @param {DOMElement} element
+     * @param {DOMElement} form
+     * @returns {boolean}
+     */
+    function isValidInteger(element, form) {
+        if ((undefined === element.value) || (null === element.value)) {
+            return false;
+        }
+
+        if (0 !== element.value % 1) {
+            showError(element, 'integer');
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Validates whether the value of an element is an number
+     * 
+     * @param {DOMElement} element
+     * @param {DOMElement} form
+     * @returns {boolean}
+     */
+    function isValidNumber(element, form) {
+        if (isNaN(parseFloat(element.value)) && isFinite(element.value)) {
+            showError(element, 'number');
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Validates whether an required element has a value
+     * 
+     * @param {DOMElement} element The element to validate
+     * @param {DOMElement form The form that contains the element to validate
+     * @returns {boolean} TRUE on error, FALSE otherwise
+     */
+    function isValidRequired(element, form) {
+        var errorFlag = true;
+
+        switch (element.type) {
+            case 'checkbox':
+            case 'radio': {
+                if (0 === $(':input[name="' + element.name + '"]:checked', form).length) {
+                    errorFlag = false;
+                }
+                ;
+
+                break;
+            }
+
+            case 'password':
+            case 'textarea':
+            case 'text': {
+                if ('' === $.trim($(element).val())) {
+                    errorFlag = false;
+                }
+
+                break;
+            }
+
+            case 'select-one': {
+                if ('' === $('option:selected', element).val()) {
+                    errorFlag = false;
+                }
+
+                break;
+            }
+
+            case 'select-multiple': {
+                if (0 === $('option:selected', element).length) {
+                    errorFlag = false;
+                }
+
+                break;
+            }
+        }
+
+        if (!errorFlag) {
+            showError(element, 'required');
+        }
+
+        return errorFlag;
+    }
+
+    /**
      * Displays the error message for an element that is not valid
      * 
      * @param {DOMElement} element
@@ -82,6 +139,7 @@
      * @returns {void}
      */
     function showError(element, type) {
+        alert("Error: " + element.name + " " + type)
         $(element).css({"border": "2px solid red"});
     }
 
@@ -105,15 +163,37 @@
                     // Remove the error css attributes o class
                     hideError(element);
 
-                    switch ($(element).attr('data-validation')) {
-                        case "required": {
-                            errorFlag = validateRequired(element, form);
-                            break;
+                    var dataValidations = $(element).attr('data-validation').split(' ');
+                    $(dataValidations).each(function(index, validation) {
+                        switch (validation) {
+                            case "email": {
+                                if ($.inArray("require", dataValidations)
+                                    || "" !== element.value
+                                ) {
+                                    errorFlag = isValidEmail(element, form);
+                                }
+                                break;
+                            }
+
+                            case "integer": {
+                                errorFlag = isValidInteger(element, form);
+                                break;
+                            }
+
+                            case "required": {
+                                errorFlag = isValidRequired(element, form);
+                                break;
+                            }
+
+                            case "number": {
+                                errorFlag = isValidNumber(element, form);
+                                break;
+                            }
                         }
-                    }
+                    });
                 });
 
-                return !errorFlag;
+                return errorFlag;
             });
         });
     }
