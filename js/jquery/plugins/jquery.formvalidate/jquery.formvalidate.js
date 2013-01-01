6a404b305351236f57f53c81734288964cd620f7
Johann Echavarría <jjppeezz@yahoo.com>
2013-01-01 02:59:17 -0500

    Array.indexOf definition for IE<9

--- js/jquery/plugins/jquery.formvalidate/jquery.formvalidate.js
+++ js/jquery/plugins/jquery.formvalidate/jquery.formvalidate.js
@@ -3,6 +3,43 @@
  * @author Alvaro José Agámez Licha, Johann Paul Echavarría Zapata
  */
 
+/*Begin of Array.indexOf definition
+https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/IndexOf#Compatibility
+*/ 
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+        "use strict";
+        if (this == null) {
+            throw new TypeError();
+        }
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (len === 0) {
+            return -1;
+        }
+        var n = 0;
+        if (arguments.length > 1) {
+            n = Number(arguments[1]);
+            if (n != n) { // shortcut for verifying if it's NaN
+                n = 0;
+            } else if (n != 0 && n != Infinity && n != -Infinity) {
+                n = (n > 0 || -1) * Math.floor(Math.abs(n));
+            }
+        }
+        if (n >= len) {
+            return -1;
+        }
+        var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+        for (; k < len; k++) {
+            if (k in t && t[k] === searchElement) {
+                return k;
+            }
+        }
+        return -1;
+    }
+}
+/*End of Array.indexOf definition*/ 
+ 
 (function ($) {
     'use strict';
 
@@ -161,6 +198,7 @@
             case "radio":
                 if (0 === $(':input[name="' + element.name + '"]:checked', element.form).length) {
                     errorFlag = false;
+                    
                 }
 
                 break;
