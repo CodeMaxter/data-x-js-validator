5c786ed71020a94574cb77db2464549b5a3f1a1f
Alvaro José Agámez Licha <alvaroagamez@outlook.com>
2013-01-31 22:48:08 -0500

    Changes and improvements
    Change:  The translations file names was changed to follow the i18n and
    i10n standards
    Change: The messages in translations files was changed from constants to
    a literal object
    Improvement: It simplified the code that chooses the appropriate error
    message for each type of error in the showError() method
    Improvement: New CSS styles for the message error

--- js/en-US.js
+++ js/en-US.js
@@ -1,12 +1,26 @@
 /* English messages configuration */
-var ATTRIBUTES_IN_CONFLICT = "Validation attributes in conflict. Please fix it.";
-var ALPHANUMERIC_MESSAGE = "It must be Alphanumeric.";
-var ALPHABETICAL_MESSAGE = "It must be alphabetical.";
-var EMAIL_MESSAGE = "It must be a correct email.";
-var ENUM_MESSAGE = "The value it not in the enum parameters.";
-var ENUM_PARAMS_MESSAGE = "Parameters were not supplied.";
-var INTEGER_MESSAGE = "It must be an integer.";
-var NUMBER_MESSAGE = "It must be number.";
-var RANGE_MESSAGE = "Invalid value for the range.";
-var REGEX_MESSAGE = "Invalid value for the regular expression.";
-var REQUIRED_MESSAGE = "It must be filled.";
+//var ATTRIBUTES_IN_CONFLICT = "Validation attributes in conflict. Please fix it.";
+//var ALPHANUMERIC_MESSAGE = "It must be Alphanumeric.";
+//var ALPHABETICAL_MESSAGE = "It must be alphabetical.";
+//var EMAIL_MESSAGE = "It must be a correct email.";
+//var ENUM_MESSAGE = "The value it not in the enum parameters.";
+//var ENUM_PARAMS_MESSAGE = "Parameters were not supplied.";
+//var INTEGER_MESSAGE = "It must be an integer.";
+//var NUMBER_MESSAGE = "It must be number.";
+//var RANGE_MESSAGE = "Invalid value for the range.";
+//var REGEX_MESSAGE = "Invalid value for the regular expression.";
+//var REQUIRED_MESSAGE = "It must be filled.";
+
+var i18n = {
+    "attributesInConflict": "Validation attributes in conflict. Please fix it.",
+    "alphanumeric" : "It must be Alphanumeric.",
+    "alphabetical" : "It must be alphabetical.",
+    "email" : "It must be a correct email.",
+    "enum" : "The value it not in the enum parameters.",
+    "enumParams" : "Parameters were not supplied.",
+    "integer" : "It must be an integer.",
+    "number" : "It must be number.",
+    "range" : "Invalid value for the range.",
+    "regex" : "Invalid value for the regular expression.",
+    "required" : "It must be filled."
+}
--- js/es-CO.js
+++ js/es-CO.js
@@ -1,12 +1,26 @@
 /* Configuración de mensajes en español */
-var ATTRIBUTES_IN_CONFLICT = "Atributos de validación en conflicto. Favor corregir.";
-var ALPHANUMERIC_MESSAGE = "Debe ser alfanumérico.";
-var ALPHABETICAL_MESSAGE = "Debe tener sólo letras.";
-var EMAIL_MESSAGE = "Debe ser un correo electrónico válido.";
-var ENUM_MESSAGE = "El valor no está en los valores enumerados permitidos.";
-var ENUM_PARAMS_MESSAGE = "No se ingresaron los parámetros de la enumeración.";
-var INTEGER_MESSAGE = "Debe ser un número entero.";
-var NUMBER_MESSAGE = "Debe ser un número";
-var RANGE_MESSAGE = "Valor por fuera del rango permitido.";
-var REGEX_MESSAGE = "Valor inválido para la expresión regular.";
-var REQUIRED_MESSAGE = "Es un campo requerido.";
+//var ATTRIBUTES_IN_CONFLICT = "Atributos de validación en conflicto. Favor corregir.";
+//var ALPHANUMERIC_MESSAGE = "Debe ser alfanumérico.";
+//var ALPHABETICAL_MESSAGE = "Debe tener sólo letras.";
+//var EMAIL_MESSAGE = "Debe ser un correo electrónico válido.";
+//var ENUM_MESSAGE = "El valor no está en los valores enumerados permitidos.";
+//var ENUM_PARAMS_MESSAGE = "No se ingresaron los parámetros de la enumeración.";
+//var INTEGER_MESSAGE = "Debe ser un número entero.";
+//var NUMBER_MESSAGE = "Debe ser un número";
+//var RANGE_MESSAGE = "Valor por fuera del rango permitido.";
+//var REGEX_MESSAGE = "Valor inválido para la expresión regular.";
+//var REQUIRED_MESSAGE = "Es un campo requerido.";
+
+var i18n = {
+    "attributesInConflict": "Atributos de validación en conflicto. Favor corregir.",
+    "alphanumeric" : "Debe ser alfanumérico.",
+    "alphabetical" : "Debe tener sólo letras.",
+    "email" : "Debe ser un correo electrónico válido.",
+    "enum" : "El valor no está en los valores enumerados permitidos.",
+    "enumParams" : "No se ingresaron los parámetros de la enumeración.",
+    "integer" : "Debe ser un número entero.",
+    "number" : "Debe ser un número.",
+    "range" : "Valor por fuera del rango permitido.",
+    "regex" : "Valor inválido para la expresión regular.",
+    "required" : "Es un campo requerido."
+}
